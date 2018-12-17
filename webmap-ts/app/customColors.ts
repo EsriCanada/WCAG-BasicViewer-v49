0128abae3c631fe9a7933c4d2279e5d22b1c769e
Horia Tudosie <htudosie@esri.ca>
2018-12-17 15:07:46 -0500

    fixed customColors functions (toHex)

--- webmap-ts/app/customColors.ts
+++ webmap-ts/app/customColors.ts
@@ -205,54 +205,35 @@ const HexToRGB = (hex: string) : {r: number, g: number, b: number} => {
   throw new Error('Bad Hex');
 }
 
+const toHex = (n: number, d: number = 2) : string => {
+  let s = n.toString(16);
+  return `${Array(d-s.length+1).join("0")}${s}`;
+}
+
 export function LightenDarkenColor(col: string, amt: number): string {
+  const usePound = col[0] == "#";
+  const RGB = HexToRGB(col);
 
-  let usePound = false;
-  if (col[0] == "#") {
-      col = col.slice(1);
-      usePound = true;
-  }
-  const num = parseInt(col, 16);
-
-  let r = (num >> 16) + amt;
+  let r = RGB.r + amt;
   if (r > 255) r = 255;
-  else if (r < 0) r = 0;
-  
-  let b = ((num >> 8) & 0x00FF) + amt;
+  else if  (r < 0) r = 0;
+
+  let b = RGB.b + amt;
   if (b > 255) b = 255;
-  else if (b < 0) b = 0;
-  
-  let g = (num & 0x0000FF) + amt;
+  else if  (b < 0) b = 0;
+
+  let g = RGB.g + amt;
   if (g > 255) g = 255;
   else if (g < 0) g = 0;
-  
-  return (usePound ? "#" : "") + (g | (b << 8) | (r << 16)).toString(16);
-}
-// export function LightenDarkenColor(col: string, amt: number): string {
-//   // console.log("LightenDarkenColor", col, amt);
-//   const usePound = col[0] == "#";
-//   const RGB = HexToRGB(col);
 
-//   let r = RGB.r + amt;
-//   if (r > 255) r = 255;
-//   else if  (r < 0) r = 0;
-
-//   let b = RGB.b + amt;
-//   if (b > 255) b = 255;
-//   else if  (b < 0) b = 0;
-
-//   let g = RGB.g + amt;
-//   if (g > 255) g = 255;
-//   else if (g < 0) g = 0;
-
-//   return (usePound?"#":"") + (g | (b << 8) | (r << 16)).toString(16);
-// }
+  return `${(usePound ? "#" : "")}${toHex(r)}${toHex(g)}${toHex(b)}`;
+  }
 
 export function MixColors(colA: string, colB: string, amountToMix:number = 0.5) : string {
   const colorChannelMixer = (colorChannelA: number, colorChannelB: number, amountToMix: number): number => {
       const channelA = colorChannelA * amountToMix;
       const channelB = colorChannelB * (1-amountToMix);
-      return channelA + channelB;
+      return Math.round(channelA + channelB);
   }
   
   const rgbA = HexToRGB(colA);
@@ -260,7 +241,8 @@ export function MixColors(colA: string, colB: string, amountToMix:number = 0.5) 
   const r = colorChannelMixer(rgbA.r, rgbB.r ,amountToMix);
   const g = colorChannelMixer(rgbA.g, rgbB.g ,amountToMix);
   const b = colorChannelMixer(rgbA.b, rgbB.b, amountToMix);
-  return "#" + (g | (b << 8) | (r << 16)).toString(16);
+  console.log("rgn", r, g, b);
+  return `#${toHex(r)}${toHex(g)}${toHex(b)}`;
 }
 
 export function isDark(col: string): boolean {
@@ -279,7 +261,7 @@ export function Negate(col: string): string {
   rgb.r = 255 - rgb.r;
   rgb.g = 255 - rgb.g;
   rgb.b = 255 - rgb.b;
-  return "#" + (rgb.g | (rgb.b << 8) | (rgb.r << 16)).toString(16);
+  return `#${toHex(rgb.r)}${toHex(rgb.b)}${toHex(rgb.g)}`;
 }
 
 export function WhiteOrBlack(col: string): string {
