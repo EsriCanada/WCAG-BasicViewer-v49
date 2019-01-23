7b15572f5be267559738dff2bbd7356df401b835
Horia Tudosie <htudosie@esri.ca>
2019-01-23 13:05:34 -0500

    Distance & Area measurements raw widgets

---
+++ webmap-ts/app/widgets/Measurement/Measurement.tsx
@@ -0,0 +1,68 @@
+/// <amd-dependency path="esri/core/tsSupport/declareExtendsHelper" name="__extends" />
+/// <amd-dependency path="esri/core/tsSupport/decorateHelper" name="__decorate" />
+
+import {subclass, declared, property} from "esri/core/accessorSupport/decorators";
+import Widget = require("esri/widgets/Widget");
+import domConstruct = require("dojo/dom-construct");
+import query = require("dojo/query");
+import dom = require("dojo/dom");
+import on = require("dojo/on");
+import domAttr = require("dojo/dom-attr");
+import domStyle = require("dojo/dom-style");
+
+import { tsx } from "esri/widgets/support/widget";
+
+import i18n = require("dojo/i18n!../nls/resources");
+import { runInThisContext } from "vm";
+
+@subclass("esri.widgets.Measurement")
+  class Measurement extends declared(Widget) {
+    @property()
+    mapView: __esri.MapView;
+
+    render() {
+        return (
+<div>
+<div class="round-border">
+        <h2>Distance Measuremnt</h2>
+        <div afterCreate={this._addDistanceMeasurement2D}></div>
+    </div>
+    <div class="round-border">
+        <h2>Area Measuremnt</h2>
+        <div afterCreate={this._addAreaMesurement2D}></div>
+    </div>
+</div>
+        )
+    }
+
+    private _addDistanceMeasurement2D = (element: Element) => {
+        require(["esri/widgets/DistanceMeasurement2D"], (DistanceMeasurement2D) => { 
+            console.log("measure", this.mapView);
+            var measurementWidget = new DistanceMeasurement2D({
+                viewModel: {
+                    view: this.mapView,
+                    unit: "kilometers",
+                    unitOptions: ["metric", "meters", "kilometers"],
+                },
+                container: element
+            })
+        });
+    }
+
+    private _addAreaMesurement2D = (element: Element) => {
+        require(["esri/widgets/AreaMeasurement2D"], (AreaMeasurement2D) => { 
+            console.log("measure", this.mapView);
+            var measurementWidget = new AreaMeasurement2D({
+                viewModel: {
+                    view: this.mapView,
+                    unit: "kilometers",
+                    unitOptions: ["metric", "square-meters", "square-kilometers", "acres", "ares", "hectares"],
+                },
+                container: element
+            })
+        });
+    }
+
+}
+
+export = Measurement;
