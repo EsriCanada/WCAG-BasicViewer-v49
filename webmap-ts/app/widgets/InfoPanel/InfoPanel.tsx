ecb88a2984015e521496ca4c050f1c39b9fba715
Horia Tudosie <htudosie@esri.ca>
2019-02-09 03:59:10 -0500

    InfoPanel content instructions

--- webmap-ts/app/widgets/InfoPanel/InfoPanel.tsx
+++ webmap-ts/app/widgets/InfoPanel/InfoPanel.tsx
@@ -25,13 +25,50 @@ import i18n = require("dojo/i18n!../nls/resources");
 
     render() {
         return (
-    <div afterCreate={this._addInfoPanel}>Info Panel</div>
+    <div afterCreate={this._addInfoPanel}>
+    	<div id="popupInfoContentWrapper" tabindex="0" style="height:100%;"  afterCreate={this._addContentPanel}>
+            <div >
+                <div id="feature_content"></div>
+            </div>
+        </div>
+        <div id="popupInfoFooter" style="display:none;">
+            <table width='100%' role='presentation' class='infoPanelFooter'>
+                <tr>
+                    <td width='33%'>
+                        <span id='locatorScore' class='locatorScore'>${i18n.popupInfo.Score} <span data-dojo-attach-point='Score'></span></span>
+                    </td>
+                    <td id='infoPanelFooterNavigation' width='34%' style='text-align:center;' role='navigation' aria-label="footerNavigation">
+                        <input type='image' src='images/icons_black/downArrow.png' aria-label={i18n.popupInfo.Prev} title={i18n.popupInfo.Prev} style='transform: rotate(90deg);' alt='Previous' class='popupInfoButton prev' data-dojo-attach-event='onclick: footerToPrev'></input>
+                        <input type='image' src='images/icons_black/downArrow.png' aria-label={i18n.popupInfo.Next} title={i18n.popupInfo.Next} style='transform: rotate(-90deg);' alt='Next' class='popupInfoButton next' data-dojo-attach-event='onclick: footerToNext'></input>
+                    </td>
+                    <td width='33%' style='text-align:right;'>
+                        <a id='locatorCopy' class='locatorCopy' tabindex="0" title={i18n.geoCoding.CopyToClipboard} data-dojo-attach-event='onclick: copyAddress'>{i18n.geoCoding.Copy}</a>
+                    </td>
+                </tr>
+            </table>
+        </div>
+    </div>
         );
     }
 
     private infoPanelElement : HTMLDivElement;
     private _addInfoPanel = (element: Element) => {
         this.infoPanelElement = element as HTMLDivElement;
+    }
+
+    private contentElement : HTMLElement;
+    private contentPanel: any;
+    private _addContentPanel = (element: Element) => {
+        this.contentElement = element as HTMLElement;
+        require(["dojox/layout/ContentPane"], (ContentPane) => { 
+            this.contentPanel = new ContentPane({
+                region: "center",
+                id: "popupInfoContent",
+                // tabindex: 0,
+            }, dom.byId("feature_content"));
+            this.contentPanel.startup();
+            this.contentPanel.set("content", i18n.popupInfo.instructions);
+        });
     }
      
 }
--- webmap-ts/app/widgets/nls/fr/resources.js
+++ webmap-ts/app/widgets/nls/fr/resources.js
@@ -126,6 +126,68 @@ define(
             symbol: "Symbole",
             "resultOf": '{0} de {1}'
         }),
+        geoCoding: {
+            "tooltips": "Infobulles",
+            "hideTooltips": "Pas infobulles",
+            "showTooltips": "Afficher les infobulles",
+            "zoomTo": "Zoomer sur l'emplacement",
+            "clear": "Effacer la sélection",
+            "noAddress": "Pas d'adresse",
+            "noAddressFound": "Aucune adresse trouvée au point",
+            "instructions": "Cliquez sur la carte pour obtenir l'adresse du lieu, <br/>ou <br/>" +
+                "<a href='#' onclick='dojo.byId(\"mapDiv\").focus();'>Aller à la carte</a> et:" +
+                "<ul>" +
+                "<li>déplacer la carte avec <myKey>flèches</myKey>, ou</li>" +
+                "<li>déplacer le curseur de carte avec <myKey>MAJ</myKey> + <myKey>flèches</myKey>, puis</li>" +
+                "<li>appuyez sur <myKey>Entrée</myKey> pour sélectionner au curseur.</li>" +
+                "</ul>",
+            "locationToAddress": "Lieu à adresse",
+            "Location": "Emplacement",
+            Copy: "Copier",
+            CopyToClipboard: "Copier dans le presse-papier",
+            "Address": "Adresse&nbsp;:",
+            "Block": "Bloc&nbsp;:",
+            "BldgName": "Nom&nbsp;du&nbsp;bâtiment&nbsp;:",
+            "LevelName": "Nom&nbsp;du&nbsp;niveau&nbsp;:",
+            "Phone": "Téléphone :",
+            "Side": "Côté :",
+            "StDir": "Rue&nbsp;Direction&nbsp;/&nbsp;Type&nbsp;:",
+            "Status": "Statut :",
+            "UnitName": "Unité :",
+            "City": "Ville&nbsp;:",
+            "CountryCode": "Code&nbsp;pays&nbsp;:",
+            "District": "District&nbsp;:",
+            "MetroArea": "Zone&nbsp;métropolitaine&nbsp;:",
+            "Neighborhood": "Quartier&nbsp;:",
+            "PlaceName": "Nom&nbsp;de&nbsp;lieu&nbsp;:",
+            "PostalCode": "Code&nbsp;Postal&nbsp;:",
+            "Region": "Région&nbsp;:",
+            "Sector": "Secteur&nbsp;:",
+            "Territory": "Territoire&nbsp;:",
+            "Type": "Type&nbsp;:",
+            "Addr_type": "Type&nbsp;d'addr&nbsp;:"
+        },
+        "addrType": {
+            PointAddress: "Adresse",
+            StreetName: "Nom de rue",
+            StreetAddress: "Adresse de rue",
+            POI: "Point d'intérêt",
+            World: "Monde",
+            Locality: "Localité",
+            Neighborhood: "Quartier",
+            City: "Ville",
+            Zone: "Zoné",
+            Bank: "Banque",
+            Park: "Parc",
+            Postal: "Code Postal",
+            College: "Collège",
+            School: "École",
+            ShoppingCenter: "Centre commercial",
+            BusinessFacility: "Facilité d'affaires",
+            County: "Département",
+            GolfCourse: "Terrain de golf",
+            Parking: "Stationnement"
+        }
 
     })
 );
