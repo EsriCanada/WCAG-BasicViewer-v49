600adc5d245004504576c65b6029830032f0ca69
Horia Tudosie <htudosie@esri.ca>
2019-05-17 12:29:47 -0400

    Cleanup

--- webmap-ts/app/widgets/AddressManager/UtilsViewModel.tsx
+++ webmap-ts/app/widgets/AddressManager/UtilsViewModel.tsx
@@ -5,7 +5,6 @@ import Accessor = require("esri/core/Accessor");
 
 import { subclass, declared, property } from "esri/core/accessorSupport/decorators";
 import MapView = require("esri/views/MapView");
-import Map = require ("esri/Map");
 import Deferred = require("dojo/Deferred");
 import FeatureLayer = require("esri/layers/FeatureLayer");
 import GraphicsLayer = require("esri/layers/GraphicsLayer");
@@ -77,16 +76,14 @@ class UtilsViewModel extends declared(Accessor) {
                 tempGraphicsLayer.destroy();
 
                 const buffer = geometryEngine.buffer(event.graphic.geometry, 5, "meters");
-                // console.log("buffer", buffer);
-
-                // console.log("BUFFER_SYMBOL", BUFFER_SYMBOL);
-                let gr = { 
+    
+                const gr = { 
                     geometry: buffer, 
                     symbol: this.BUFFER_SYMBOL
                 };
                 // console.log("gr", gr);
 
-                this.mapView.graphics.add(gr as any);
+                this.mapView.graphics.add(gr);
           
                 const q = this.roadsLayer.createQuery();
                 q.outFields = ["*"];
@@ -102,19 +99,17 @@ class UtilsViewModel extends declared(Accessor) {
                             const roadMarker = geometryEngine.buffer(roads[0].geometry, 2.5, "meters");
                             const roadGraphic = { geometry: roadMarker, symbol: this.BUFFER_SYMBOL, attributes: roads[0].attributes};
                             // 
-                            setTimeout(() => { 
-                                this.mapView.graphics.removeAll(); 
-                            }, 250);
+                            setTimeout(() => { this.mapView.graphics.removeAll(); }, 250);
                             deferred.resolve(roads[0]);
                         } else {
-                            setTimeout(lang.hitch(function() { this.mapView.graphics.removeAll() }), 250);
+                            setTimeout(() =>  { this.mapView.graphics.removeAll(); }, 250);
                             deferred.cancel("Too many or no matches")
                         }
                     }),
                     error => {
                         // console.error("PICK_ROAD", error);
                         deferred.cancel(error);
-                        setTimeout(lang.hitch(function() { this.mapView.graphics.removeAll() }), 250);
+                        setTimeout(() => { this.mapView.graphics.removeAll(); }, 250);
                     }
                 );
 
