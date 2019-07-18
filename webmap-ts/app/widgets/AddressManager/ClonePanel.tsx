8616919bad3cdf612d8c57d8c4954dac9601ba7d
Horia Tudosie <htudosie@esri.ca>
2019-07-18 18:42:08 -0400

    Cleanup

--- webmap-ts/app/widgets/AddressManager/ClonePanel.tsx
+++ webmap-ts/app/widgets/AddressManager/ClonePanel.tsx
@@ -214,13 +214,13 @@ import { Geometry } from "esri/geometry";
                         </td>
                     </tr>
                     <tr>
-                        <th><label for="StreeNumStart">Street # Start:</label></th>
+                        <th><label for="StreeNumStart">Address # Start:</label></th>
                         <td>
                             <input type="number" class="numInput" id="StreeNumStart" min="1" step="1" name="StreeNumStart" value="1" afterCreate={this._addStreeNumStart}/>
                         </td> 
                     </tr>
                     <tr>
-                        <th><label for="StreeNumStep">Street # Step:</label></th>
+                        <th><label for="StreeNumStep">Address # Step:</label></th>
                         <td>
                             <input type="number" class="numInput" id="StreeNumStep" min="1" max="8" step="1" name="StreeNumStep" value="2" afterCreate={this._addStreeNumStep}/>
                         </td> 
